e9e3e3df9f1388682ed4fc6ebb8c0d14d5dea9e3
Sanpreet Singh <sanpreet.india@gmail.com>
2023-03-10 13:28:49 +0530

    commiting the while loop

--- Control Statement & Loops/switchStmt.js
+++ Control Statement & Loops/switchStmt.js
@@ -13,17 +13,19 @@ if (area === "circle"){
     console.log("please enter a valid date");
 }*/
 
-var area = "circle";
+var area = "square";
 var PI = 3.142, l=5, b=4, r=3;
 
 switch(area){
     case "circle" :
         console.log("the area of circle is :" + PI*r**2);
-        
+        break;
     case "triangle" :
         console.log("the area of triangle is" + (l*b)/2);
+        break;
     case "rectangle" :
         console.log("the area of rectangle is" + (l*b));
+        break;
     default :
     console.log("please enter a valid date");
 }   
---
+++ Control Statement & Loops/whileLoop.js
@@ -0,0 +1,11 @@
+// var number = 10;
+// while(number <=10){
+//     console.log(number);  // it was became infinite loop since we did not given the stop value
+// }
+
+var number = 0;
+// it's a block scope,, it will first the condition then eneter to the block.
+while(number <=55){
+    console.log(number);
+    number++;
+}
