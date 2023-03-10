13ce68b977107f58060770e3529d45d9394e72ee
Sanpreet Singh <sanpreet.india@gmail.com>
2023-03-10 17:28:01 +0530

    commiting function return

---
+++ Functions/fnctReturn.js
@@ -0,0 +1,7 @@
+function sum (a,b){
+    return total = a+b;
+}
+
+var funExp = sum(10,35);
+
+console.log("The sum of numbers is " +funExp);
--- Functions/functionStmt.js
+++ Functions/functionStmt.js
@@ -10,10 +10,20 @@
 
 // sum();
 
-function sum(){
+/*function sum(){
     var a = 10, b = 550;
     var total = (a+b); // the total value can work with or without ()
     console.log (total);
 }
 
-sum();
+sum();*/
+
+function sum(a,b){          // in function declaration ()"parenthesis" what we pass is called parameters
+    //var a = 10, b = 550;
+    var total = (a+b); // the total value can work with or without ()
+    console.log (total);
+}
+
+// sum();
+sum(45,20);                 //in function calling ()"parenthesis" what we pass is called argument
+sum(25,33);
