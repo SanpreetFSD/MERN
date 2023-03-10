06ffd79e07a72bf5f95bf2846f2e83e04886e18b
Sanpreet Singh <sanpreet.india@gmail.com>
2023-03-10 14:20:20 +0530

    commiting for loop

--- Control Statement & Loops/doWhileLoop.js
+++ Control Statement & Loops/doWhileLoop.js
@@ -4,3 +4,7 @@ do{
     console.log(num);
     num++;
 }while(num <= 15);  // do while loop checks the condition at last and gives the output before
+
+
+// The initialization of the variable may occur within or before the execution of the loop. 
+//The while loop is an entry-controlled type of loop. The do-while loop is an exit-controlled type of loop.
---
+++ Control Statement & Loops/forLoop.js
@@ -0,0 +1,20 @@
+/*for(initializer; Condition ; iteration){
+    //code to be exectued
+}*/
+
+/*var index = 0;
+for(index = 0; index <= 10; index++){
+    console.log(index);
+} */
+
+// print table of 9
+
+// for(var num = 1; num <=10; num++)
+// {   var tableOf = 9;
+//     console.log(tableOf + " * " + num + " = " + (tableOf*num));
+// }
+
+for(var num = 1; num <=10; num++)
+{   var tableOf = "10";
+    console.log(tableOf + " * " + num + " = " + (tableOf*num));
+}
