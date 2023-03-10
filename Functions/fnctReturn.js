1101f4e99008339dacb6d1775d1daf8e51c590a8
Sanpreet Singh <sanpreet.india@gmail.com>
2023-03-10 19:47:19 +0530

    commit funct retrun advance

--- Functions/fnctReturn.js
+++ Functions/fnctReturn.js
@@ -1,7 +1,14 @@
+// Anonymous function expression and anonymous function
+
 function sum (a,b){
     return total = a+b;
 }
 
 var funExp = sum(10,35);
 
+var funExp1 = sum(20, 40)
+
 console.log("The sum of numbers is " +funExp);
+console.log("The sum of numbers is " +funExp1);  // this way we can use the function many times
+
+console.log(funExp > funExp1);  // we can also check the greater than and less than
