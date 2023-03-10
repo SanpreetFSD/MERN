49171915c5f183ce6828eab99a34aec696796e93
Sanpreet Singh <sanpreet.india@gmail.com>
2023-03-10 14:36:42 +0530

    commiting function

---
+++ Functions/functionStmt.js
@@ -0,0 +1,19 @@
+// first we have to define the function
+
+// function sum(){
+//     var a = 10, b = 20;
+//     var total = a+b;
+//     console.log (total);
+// }
+
+// calling a function
+
+// sum();
+
+function sum(){
+    var a = 10, b = 550;
+    var total = (a+b); // the total value can work with or without ()
+    console.log (total);
+}
+
+sum();
