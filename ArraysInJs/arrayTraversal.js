4dc277c128aac3fc0be6f20724ab0cf6a6119a1d
Sanpreet Singh <sanpreet.india@gmail.com>
2023-03-12 10:17:44 +0530

    commiting array traversal file

---
+++ ArraysInJs/arrayTraversal.js
@@ -0,0 +1,7 @@
+// Traversal in Array
+
+var myFriends = ['Ramesh', 'Suresh', 'Naresh', 'Ganesh'];
+
+console.log(myFriends[1]);
+
+console.log(myFriends[2,0]); // if we check for more than one index, the array will show the lates of last index value
