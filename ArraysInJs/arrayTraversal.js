48f1aa24fe35184a004c2cc09279e28edc23180e
Sanpreet Singh <sanpreet.india@gmail.com>
2023-03-12 11:06:41 +0530

    commiting later files

--- ArraysInJs/arrayTraversal.js
+++ ArraysInJs/arrayTraversal.js
@@ -1,7 +1,20 @@
 // Traversal in Array
 
+//var myFriends = ['Ramesh', 'Suresh', 'Naresh', 'Ganesh', 'Ramesh2', 'Suresh2', 'Naresh2', 'Ganesh2'];
+
+//console.log(myFriends[1]);
+
+//console.log(myFriends[2,0]); // if we check for more than one index, the array will show the lates of last index value
+
+// to check the length of Array elements
+
+// console.log(myFriends.length);
+// console.log(myFriends.length -1);
+
+// us of for loop in array to check the array elements
+
 var myFriends = ['Ramesh', 'Suresh', 'Naresh', 'Ganesh'];
 
-console.log(myFriends[1]);
-
-console.log(myFriends[2,0]); // if we check for more than one index, the array will show the lates of last index value
+for (var i=0; i<myFriends.length; i++){
+    console.log(myFriends[i]);
+}
