a7bee369068ab02ad7b5c3226021cae474649284
Sanpreet Singh <sanpreet.india@gmail.com>
2023-03-18 15:30:00 +0530

    commiting push and unshift

---
+++ ArraysInJs/ClassNobjcts.js
@@ -0,0 +1,13 @@
+class hello{
+    message(){
+        console.log("Hello Everyone");
+    }
+    sorry(){
+        console.log("Sorry Everyone");
+    }
+}
+
+let a = new hello();
+
+a.message();
+a.sorry();
---
+++ ArraysInJs/pushUnshift.js
@@ -0,0 +1,14 @@
+// const animals = ["dog", "goat", "sheep"];
+// const count = animals.push("cow");
+
+// console.log(animals);
+// console.log(count);
+
+// .push method adds any value at last of the array
+// .push returns the length of the array
+
+const animals = ["dog", "goat", "sheep"];
+const count = animals.unshift("cow");
+
+console.log(animals);
+console.log(count); // .unshift method pusth the value at begining of the array
