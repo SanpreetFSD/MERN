0422bfac24c66b04123702a6e1713fcab853875e
Sanpreet Singh <sanpreet.india@gmail.com>
2023-03-18 15:49:03 +0530

    commiting the pop n shift

---
+++ ArraysInJs/popNshift.js
@@ -0,0 +1,16 @@
+// unshift method
+
+// const vegi = ["broccoli", "cauliflorwer", "brinjal", "tomato", "cabbage"];
+
+// console.log(vegi); // here it is giving the complete array length n vlues
+// console.log(vegi.pop()); // here is is removing the last element from the array and reducing the length of arry
+// console.log(vegi); // it is now showing the new array 
+
+
+// shift method
+
+const vegi = ["broccoli", "cauliflorwer", "brinjal", "tomato", "cabbage"];
+
+console.log(vegi); 
+console.log(vegi.shift());
+console.log(vegi); 
--- ArraysInJs/pushUnshift.js
+++ ArraysInJs/pushUnshift.js
@@ -7,8 +7,13 @@
 // .push method adds any value at last of the array
 // .push returns the length of the array
 
-const animals = ["dog", "goat", "sheep"];
-const count = animals.unshift("cow");
+// const animals = ["dog", "goat", "sheep"];
+// const count = animals.unshift("cow");
 
-console.log(animals);
-console.log(count); // .unshift method pusth the value at begining of the array
+// console.log(animals);
+// console.log(count); // .unshift method pusth the value at begining of the array
+
+const myNumber = [1,2,3,5];
+
+myNumber.unshift(4,6);
+console.log(myNumber); // here we can see "unshift" is putting value at begning but not is sequence,if we need to put any value in-between.
